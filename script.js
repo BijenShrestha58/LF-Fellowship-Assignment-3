1a749f29c97f35af60a5c5ed0357ac3af056fbc7
Bijen <gr8bijen@gmail.com>
2024-06-10 16:12:21 +0545

    Feature: Adds elastic collision

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const BOUNDARY_X_MIN = 0;
+const BOUNDARY_X_MAX = 1000;
+
+const BOUNDARY_Y_MIN = 0;
+const BOUNDARY_Y_MAX = 700;
+
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
@@ -42,21 +48,15 @@ function getRandomColor() {
 }
 
 const boundary = document.createElement("div");
-boundary.style.width = "90vw";
-boundary.style.height = "90vh";
+boundary.style.width = `${BOUNDARY_X_MAX}px`;
+boundary.style.height = `${BOUNDARY_Y_MAX}px`;
 boundary.style.border = "1px solid #ccc";
 boundary.style.position = "relative";
-boundary.style.top = "50%";
-boundary.style.left = "50%";
-boundary.style.transform = "translate(-50%,-50%)";
+// boundary.style.top = "50%";
+// boundary.style.left = "50%";
+// boundary.style.transform = "translate(-50%,-50%)";
 
 document.body.appendChild(boundary);
-
-const BOUNDARY_X_MIN = 0;
-const BOUNDARY_X_MAX = boundary.clientWidth;
-
-const BOUNDARY_Y_MIN = 0;
-const BOUNDARY_Y_MAX = boundary.clientHeight;
 
 class Ball {
   constructor(x = 0, y = 0, r = 20, color = "#000", dx = 1, dy = 1) {
@@ -66,85 +66,120 @@ class Ball {
     this.dx = dx;
     this.dy = dy;
     this.color = color;
-    this.cx = x + r;
-    this.cy = y + r;
 
     this.element = document.createElement("div");
 
     this.element.style.width = `${this.r * 2}px`;
     this.element.style.height = `${this.r * 2}px`;
-    this.element.style.transform = `translate(${this.x}px,${this.y}px)`;
     this.element.style.position = `absolute`;
     this.element.style.background = this.color;
     this.element.style.borderRadius = "50%";
   }
 
-  ballMovement() {
+  ballMovement(delta) {
+    this.x = this.x + (this.dx * delta) / 10;
+    this.y = this.y + (this.dy * delta) / 10;
     this.element.style.transform = `translate(${this.x}px,${this.y}px)`;
   }
 
   isWallCollision() {
-    if (this.y + 2 * this.r >= BOUNDARY_Y_MAX || this.y <= BOUNDARY_Y_MIN) {
-      this.dy = this.dy * -1;
+    if (this.x <= BOUNDARY_X_MIN) {
+      this.x = Math.max(0, this.x);
+      this.dx *= -1;
     }
-    if (this.x + 2 * this.r >= BOUNDARY_X_MAX || this.x <= BOUNDARY_X_MIN) {
-      this.dx = this.dx * -1;
+    if (this.x + 2 * this.r >= BOUNDARY_X_MAX) {
+      this.x = Math.min(BOUNDARY_X_MAX - 2 * this.r, this.x);
+      this.dx *= -1;
     }
-    this.x = this.x + this.dx * 5;
-    this.y = this.y + this.dy * 5;
+    if (this.y <= BOUNDARY_Y_MIN) {
+      this.y = Math.max(0, this.y);
+      this.dy *= -1;
+    }
+    if (this.y + 2 * this.r >= BOUNDARY_Y_MAX) {
+      this.y = Math.min(BOUNDARY_Y_MAX - 2 * this.r, this.y);
+      this.dy *= -1;
+    }
+
+    // if (this.y + 2 * this.r >= BOUNDARY_Y_MAX || this.y <= BOUNDARY_Y_MIN) {
+    //   this.dy = this.dy * -1;
+    // }
+    // if (this.x + 2 * this.r >= BOUNDARY_X_MAX || this.x <= BOUNDARY_X_MIN) {
+    //   this.dx = this.dx * -1;
+    // }
   }
 
   isBallCollision(ball2) {
     if (
-      (this.x - ball2.x) ** 2 + (this.y - ball2.y) ** 2 <=
-      (this.r + ball2.r) ** 2
+      Math.hypot(
+        this.x + this.r - ball2.x - ball2.r,
+        this.y + this.r - ball2.y - ball2.r
+      ) <=
+      this.r + ball2.r
     ) {
-      this.isOverlap(ball2);
-      let temp = this.dx;
-      this.dx = ball2.dx;
-      ball2.dx = temp;
+      console.log("colliding");
+      let newAdx =
+        ((this.r ** 1 - ball2.r ** 1) / (this.r ** 1 + ball2.r ** 1)) *
+          this.dx +
+        ((2 * ball2.r ** 1) / (this.r ** 1 + ball2.r ** 1)) * ball2.dx;
+      let newAdy =
+        ((this.r ** 1 - ball2.r ** 1) / (this.r ** 1 + ball2.r ** 1)) *
+          this.dy +
+        ((2 * ball2.r ** 1) / (this.r ** 1 + ball2.r ** 1)) * ball2.dy;
+      let newBdx =
+        ((2 * this.r ** 1) / (this.r ** 1 + ball2.r ** 1)) * this.dx +
+        ((ball2.r ** 1 - this.r ** 1) / (this.r ** 1 + ball2.r ** 1)) *
+          ball2.dx;
+      let newBdy =
+        ((2 * this.r ** 1) / (this.r ** 1 + ball2.r ** 1)) * this.dy +
+        ((ball2.r ** 1 - this.r ** 1) / (this.r ** 1 + ball2.r ** 1)) *
+          ball2.dy;
 
-      temp = this.dy;
-      this.dy = ball2.dy;
-      ball2.dy = temp;
+      this.dx = newAdx;
+      this.dy = newAdy;
+      ball2.dx = newBdx;
+      ball2.dy = newBdy;
+      this.isOverlap(ball2);
+
+      // let temp = this.dx;
+
+      // temp = this.dy;
+      // this.dy = ball2.dy;
+      // ball2.dy = temp;
     }
   }
 
   isOverlap(ball2) {
-    let distance = Math.sqrt((ball2.x - this.x) ** 2 + (ball2.y - this.y) ** 2);
+    let distance = Math.sqrt(
+      (ball2.x + ball2.r - this.x - this.r) ** 2 +
+        (ball2.y + ball2.r - this.y - this.r) ** 2
+    );
     let distanceRequired = ball2.r + this.r;
-    if (distance >= distanceRequired) return;
-
+    if (distanceRequired < distance) return; //no collision
     let overlap = distanceRequired - distance;
+    this.x -= (overlap * (ball2.x - this.x)) / distance;
+    this.y -= (overlap * (ball2.y - this.y)) / distance;
+    ball2.x += (overlap * (ball2.x - this.x)) / distance;
+    ball2.y += (overlap * (ball2.y - this.y)) / distance;
 
-    let correctionX = (overlap * (ball2.x - this.x)) / distance;
-    let correctionY = (overlap * (ball2.y - this.y)) / distance;
-
-    this.x -= correctionX / 2;
-    this.y -= correctionY / 2;
-
-    ball2.x += correctionX / 2;
-    ball2.y += correctionY / 2;
-
-    this.x = Math.max(this.r, Math.min(this.x, BOUNDARY_X_MAX - this.r * 2));
-    this.y = Math.max(this.r, Math.min(this.y, BOUNDARY_Y_MAX - this.r * 2));
-    ball2.x = Math.max(
-      ball2.r,
-      Math.min(ball2.x, BOUNDARY_X_MAX - ball2.r * 2)
-    );
-    ball2.y = Math.max(
-      ball2.r,
-      Math.min(ball2.y, BOUNDARY_Y_MAX - ball2.r * 2)
-    );
+    // this.x = Math.max(this.r, Math.min(this.x, BOUNDARY_X_MAX - this.r * 2));
+    // this.y = Math.max(this.r, Math.min(this.y, BOUNDARY_Y_MAX - this.r * 2));
+    // ball2.x = Math.max(
+    //   ball2.r,
+    //   Math.min(ball2.x, BOUNDARY_X_MAX - ball2.r * 2)
+    // );
+    // ball2.y = Math.max(
+    //   ball2.r,
+    //   Math.min(ball2.y, BOUNDARY_Y_MAX - ball2.r * 2)
+    // );
   }
 }
 
-const BALL_COUNT = 20;
+const BALL_COUNT = 200;
 
 const ballArray = [];
 
 for (let i = 0; i < BALL_COUNT; i++) {
-  let radius = getRandomInt(15, 30);
+  let radius = getRandomInt(5, 30);
   let color = getRandomColor();
   let [left, top] = getRandomPosition(
     BOUNDARY_X_MIN,
@@ -160,18 +195,22 @@ for (let i = 0; i < BALL_COUNT; i++) {
   ballArray.push(ball);
 }
 
-setInterval(() => {
-  ballArray.forEach((ball) => {
-    ball.isWallCollision();
-    ball.ballMovement();
-  });
-  for (let i = 0; i < ballArray.length; i++) {
-    for (let j = 0; j < ballArray.length; j++) {
-      if (ballArray[i] === ballArray[j]) break;
-      ballArray[i].isBallCollision(ballArray[j]);
-      ballArray[i].isOverlap(ballArray[j]);
+function update() {
+  {
+    for (let i = 0; i < ballArray.length - 1; i++) {
+      for (let j = i + 1; j < ballArray.length; j++) {
+        // if (ballArray[i] === ballArray[j]) break;
+        ballArray[i].isBallCollision(ballArray[j]);
+        // ballArray[i].isOverlap(ballArray[j]);
+      }
     }
+    ballArray.forEach((ball) => {
+      ball.isWallCollision();
+    });
+    ballArray.forEach((ball) => ball.ballMovement(1000 / 60));
+    setTimeout(update, 1000 / 60);
   }
-  ballArray.forEach((ball) => ball.ballMovement());
-}, 1000 / 60);
+}
+
+setTimeout(update, 1000 / 60);
 // console.log(ballArray);
